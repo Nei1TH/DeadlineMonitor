dcb58ad118b3752aad9845fe57d455f2a97c2f92
NeilHcode <tanghh5273@gmail.com>
2025-11-19 22:26:42 +0800

    Initial Commit

---
+++ Deadline/DeadlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeadlineApp.swift
+//  Deadline
+//
+//  Created by Neil on 19/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct DeadlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DeadlineTests/DeadlineTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeadlineTests.swift
+//  DeadlineTests
+//
+//  Created by Neil on 19/11/2025.
+//
+
+import Testing
+@testable import Deadline
+
+struct DeadlineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
